a3175d948dc9a7e9fc53db83875c2dbd32ca8b8d
gunjan543 <gunjanagarwal54321@gmail.com>
2021-04-21 22:33:37 +0530

    2nd project

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el:'#vue-app',
     data:{
        name:'Shaun',
-       job: 'Ninja'
+       job: 'Ninja',
+       website: 'https://github.com/gunjan543/vue',
+       websiteTag: '<a href="https://github.com/gunjan543/vue">The Website</a>'
     },
     methods:{
         greet:function(time){
